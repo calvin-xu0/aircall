90557895e2c54348728f476077523e94935a3a95
Calvin Xu <calvin.xu.jy@gmail.com>
2021-11-19 21:34:34 -0500

    Initialize styling

---
+++ src/ActivityButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./css/activityButton.css";
+
+export default function ActivityButton(props) {
+  const {created_at, direction, from, to, via, call_type} = props;
+
+  const dateObj = new Date(created_at);
+  const [hour, minute] = [dateObj.getUTCHours(), dateObj.getMinutes()];
+
+  return (
+    <div className="activity-button">
+      <div className="activity-direction">
+        {direction === "outbound" && "O" || direction === "inbound" && "I"}
+      </div>
+      <div className="activity-summary">
+        <div className="activity-from">
+          {from}
+        </div>
+        <div>
+          tried to call on {to}
+        </div>
+      </div>
+      <div className="activity-time">
+        {hour > 12 ? `${hour - 12}:${minute}` : `${hour}:${minute}`}
+        <span className="time-suffix">
+          {hour > 12 ? "PM" : "AM"}
+        </span>
+      </div>
+    </div>
+  )
+};
