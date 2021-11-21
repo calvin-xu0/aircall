0dee088714cba4957c995b5a2bb6b19f36799f94
Calvin Xu <calvin.xu.jy@gmail.com>
2021-11-21 03:59:27 -0500

    Indicate call type in feed by colour

--- src/ActivityButton.jsx
+++ src/ActivityButton.jsx
@@ -3,14 +3,21 @@ import { NavLink } from "react-router-dom";
 import "./css/activityButton.css";
 
 export default function ActivityButton(props) {
-  const {id, created_at, direction, from, to} = props;
+  const {id, created_at, direction, from, to, call_type} = props;
 
   const dateObj = new Date(created_at);
   const [hour, minute] = [dateObj.getUTCHours(), dateObj.getMinutes()];
 
+  const activityClass = () => {
+    const output = ["activity-direction"];
+    output.push(`activity-${call_type}`)
+    console.log(output)
+    return output.join(' ');
+  }
+
   return (
     <NavLink to={`/activity/${id}`} className="activity-button">
-      <div className="activity-direction">
+      <div className={activityClass()}>
         {direction === "outbound" && "📞→" || direction === "inbound" && "📞←"}
       </div>
       <div className="activity-summary">
